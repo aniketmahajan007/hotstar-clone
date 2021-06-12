60446e603c1c701dc53398fae8e88caf0395b4ef
Aniket Mahajan <007aniketmahajan@gmail.com>
2021-06-12 14:01:31 +0530

    header init

---
+++ src/component/Header/Header.js
@@ -0,0 +1,45 @@
+import React from "react";
+import './Header.css';
+import Logo from '../../asset/images/logo.svg';
+import Homeicon from  '../../asset/images/home-icon.svg';
+import Searchicon from  '../../asset/images/search-icon.svg';
+import Watchlisticon from  '../../asset/images/watchlist-icon.svg';
+import Originalsicon from  '../../asset/images/original-icon.svg';
+import Movieicon from  '../../asset/images/movie-icon.svg';
+import SeriesIcon from  '../../asset/images/series-icon.svg';
+
+function Header(){
+    return(
+        <div className="Header">
+            <img className="header__logo" src={Logo} alt="Hotstar Logo" />
+            <div className="Nav_menu">
+                <a>
+                    <img src={Homeicon} alt="Home" />
+                    <span>HOME</span>
+                </a>
+                <a>
+                    <img src={Searchicon} alt="Search" />
+                    <span>SEARCH</span>
+                </a>
+                <a>
+                    <img src={Watchlisticon} alt="Watch list" />
+                    <span>WATCHLIST</span>
+                </a>
+                <a>
+                    <img src={Originalsicon} alt="Original" />
+                    <span>ORIGINALS</span>
+                </a>
+                <a>
+                    <img src={Movieicon} alt="Movies" />
+                    <span>MOVIES</span>
+                </a>
+                <a>
+                    <img src={SeriesIcon} alt="Series" />
+                    <span>SERIES ICON</span>
+                </a>
+            </div>
+            <img src="https://lh3.googleusercontent.com/ogw/ADea4I4e38gRgi_ih1EcgKnR1_K8MJTWwTOfSo--julzfQ=s83-c-mo" alt="User Avatar" className="UserAvatar" />
+        </div>
+    )
+}
+export default Header
---
+++ src/component/Home/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Home.css';
+function Home(){
+    return(
+        <div className="Home">
+            U am home
+        </div>
+    )
+}
+export default Home
