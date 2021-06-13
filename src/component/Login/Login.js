7eae4dc03e422860321847b5bbb3cf123dc04450
Aniket Mahajan <007aniketmahajan@gmail.com>
2021-06-13 22:11:50 +0530

    Login Page design ready

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -1,10 +1,21 @@
 import React from "react";
 import './Login.css';
+import LoginBg from '../../asset/images/login-background.jpg';
+import CTone from '../../asset/images/cta-logo-one.svg';
+import CTtwo from '../../asset/images/cta-logo-two.png';
 
 function Login(){
     return (
         <div className="Login">
-            <h3>I am Login :P</h3>
+            <img className="LoginBg" src={LoginBg} alt="Bg Login" />
+            <div className="CenterElement">
+                <img src={CTone} alt="Logo"/>
+                <button>GET ALL HERE</button>
+                <p>Get Premier Access to Raya and the Last Dragon for an additional fee with a Disney+ subscription.
+                    <br/> As of 03/26/21, the price of Disney+ and The Disney Bundle will increase by $1.
+                </p>
+                <img src={CTtwo} alt="All logi"/>
+            </div>
         </div>
     )
 }
